5ff70efa53b9e8e3ce611b6208f1c763c2b0c4a8
Ben Warzeski <bwarzesk@gmail.com>
2023-06-26 14:38:17 +0000

    chore: remove stale code

--- src/utils/stateFactory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import StrictDict from './StrictDict';
-
-const stateFactory = (mapping) => StrictDict(Object.keys(StrictDict(mapping)).reduce(
-  (curr, stateKey) => ({ ...curr, [stateKey]: (val) => React.useState(val) }),
-  {},
-));
-
-export default stateFactory;
